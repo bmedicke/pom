f43d3d0983c595ee69868af6bdd6203e42c1a2ad
Benjamin Medicke <bmedicke@gmail.com>
2022-05-15 10:43:14 +0200

    use localhost

--- pomweb/static/pom.js
+++ pomweb/static/pom.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const ws = new WebSocket("ws://192.168.8.171:8421/ws");
+const ws = new WebSocket("ws://127.0.0.1:8421/ws");
 ws.addEventListener("message", (event) => {
   const payload = JSON.parse(event.data);
   var seconds = payload.Remaining / 1e9;
